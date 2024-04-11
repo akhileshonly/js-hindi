1873f33d22729ad3ba3ae2b4c9444114f662cd88
Akhilesh Yadav <67005687+akhileshonly@users.noreply.github.com>
2024-04-11 13:27:08 +0000

    variable learnt

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,9 @@
+const accountId = 123456
+var accountEmailId = "test@gm.com"
+let accountPassword = "trtr1235"
+let accountCity = "Noida"
+
+// accountId = 2 //not allowed
+let accountState
+console.log(accountId);
+console.table([accountId, accountEmailId, accountPassword, accountCity, accountState]);
