065de2963571a2361204856c8d0fb4858494db3e
Akhilesh Yadav <67005687+akhileshonly@users.noreply.github.com>
2024-04-11 14:03:04 +0000

    datatypes learnt

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"
+
+// alert (3+3) we are using node js not browser, this code can excute only in browser.
+
+console.log(3 + 3);
+console.log("hello akhilesh how are you")
+
+let name = "Akhilesh" 
+let age = 32 
+let isLoggedIn = false 
+
+// DataTypes
+
+//string datatype
+// number datatype
+// boolean datatype
+// null => standalone value
+// undefined => no vlaue has been assigned
+// symbol => its used for some uniqueness
+
+// object
+
+console.log(typeof age); // string
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
