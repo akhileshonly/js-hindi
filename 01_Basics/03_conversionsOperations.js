c9669f6549e23312b8c8ae378e0432a7e39ea3b1
Akhilesh Yadav <67005687+akhileshonly@users.noreply.github.com>
2024-04-12 07:19:31 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionsOperations.js
@@ -0,0 +1,26 @@
+let score = "33abc"
+let nameOfPerson = null
+let numberIs = "77"
+
+//console.table ([typeof score, typeof nameOfPerson]);
+//console.log(typeof score);
+
+let run = undefined
+let isLoggedIn = 33
+
+let theNumber = Number(nameOfPerson)
+
+let isNumberLoggedIn = Boolean(isLoggedIn)
+
+let someString = String(isLoggedIn)
+
+console.log(typeof someString);
+
+//console.log(typeof isLoggedIn);
+
+//findtheType = [score, nameOfPerson, run, isLoggedIn]
+
+//console.table ([typeof score, typeof nameOfPerson, typeof(run), typeof(isLoogedIn)]);
+//console.table([findtheType]);
+//console.log(findtheType);
+
