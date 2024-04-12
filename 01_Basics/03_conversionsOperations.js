d9181446639dc3ed58f2be6c2ac69027b3c86272
Akhilesh Yadav <67005687+akhileshonly@users.noreply.github.com>
2024-04-12 15:49:15 +0000

    conversions & comparison

--- 01_Basics/03_conversionsOperations.js
+++ 01_Basics/03_conversionsOperations.js
@@ -14,13 +14,44 @@ let isNumberLoggedIn = Boolean(isLoggedIn)
 
 let someString = String(isLoggedIn)
 
-console.log(typeof someString);
+//console.log(typeof someString);
 
 //console.log(typeof isLoggedIn);
 
 //findtheType = [score, nameOfPerson, run, isLoggedIn]
 
-//console.table ([typeof score, typeof nameOfPerson, typeof(run), typeof(isLoogedIn)]);
-//console.table([findtheType]);
-//console.log(findtheType);
+// console.table ([typeof score, typeof nameOfPerson, typeof(run), typeof(isLoogedIn)]);
+// console.table([findtheType]);
+// console.log(findtheType);
 
+// ======================Operations========================
+
+let str1 = "Hello"
+let str2 = " Akhilesh"
+let str3 = str1+str2
+console.log(str3);
+
+// console.log(1 + "3"); //output: 13 (If string is last then first operations made and then added to string)
+
+// console.log("1" + 2); //output: 12 (If string is first then all values will be treated as string and not operations wiil be made)
+
+// console.log(1+"2"+"3"); //output: 123 (If string is second or last then first operations made and then added to string)
+
+// console.log("1"+2+4); //outout: 124 (If string is first then all values will be treated as string and no operations wiil be made)
+
+// console.log(1+3+"4"); //output: 44  (Since the first two values are not string then there is an operation between two and then convert into string as per last)  
+
+// console.log((1+2)*3); //output: 9 Try to use paranthesis between the values
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++
+
+// console.log(gameCounter); //output will be 101 but still the gameCounter variable has its assigned value is 100
+
+++gameCounter
+
+console.log(gameCounter); // output will be 101 and it will assign gameCounter to 101
---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,20 @@
+
+// console.log(2 == 2);
+// console.log(2 != 2);
+// console.log(2 >= 2);
+// console.log(3 != 4);
+// console.log(3 < 3);
+let x = 2
+let y = "2"
+// console.log(x == y) // “==“ is used to compare both values only.
+// console.log(x === y) // “===“ is used to compare both values and types.
+
+console.log("2">1);
+console.log("02">1);
+
+// console.log(null >=0);
+// console.log(null == 0);
+// console.log(null > 0);
+
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
