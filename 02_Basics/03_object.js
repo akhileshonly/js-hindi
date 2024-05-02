fc1db9933127c2b43b969689ae2bb29c42a6c942
Akhilesh Yadav <67005687+akhileshonly@users.noreply.github.com>
2024-05-02 08:22:59 +0000

    Objects

--- 02_Basics/03_object.js
+++ 02_Basics/03_object.js
@@ -1,19 +1,32 @@
-const mysm = new Symbol("keyt1") // symbol declaration
+const mysm = Symbol("keyt1") // symbol declaration
 
 const userData = {
     name: "akhilesh",
     email: "akhilesh@enthuons.com",
     location: "noida",
-    [mysm]: "keyt1"
+    [mysm]: "keyt1",
     age: 18,
     isusrlogged: false,
     lastLoginDays: ["monday", "thursday", "saturday"]
 };
 
-console.log(userData.name);
-console.log(userData.lastLoginDays[0]);
-
+// console.log(userData.name);
+// console.log(userData.lastLoginDays[0]);
+// Object.freeze(userData);
 userData.email = "akh@hgm.com"
+//Object.freeze(userData.email);
+//userData.email = "akhilesh@hotmail.com"
+//Object.freeze(userData.email);
 userData.location = "delhi"
 
-console.log(userData.email);
+console.log(userData);
+
+userData.greeting = function(){
+console.log("Hello Akhilesh Welcome to JS")
+};
+
+userData.greeting2 = function(){
+console.log(`hello ${this.name} welcome to ${this.location}`)
+}
+console.log(userData.greeting()); 
+//console.log(userData.greeting2()); 
